d41aad6b22a864dbf44cf58b82d1482a93912d2a
Teo1188 <tadeogaviria@gmail.com>
2025-07-05 23:29:56 -0500

    Modificacion Axios conexion to backend produccion

--- Client/src/api/axiosInstance.js
+++ Client/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5091'
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5091',
+    // Puedes cambiar la URL base según tu entorno de desarrollo o producción
 });
 
 api.interceptors.request.use((config)=>{
